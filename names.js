ca20c8419a5cb3ec172d8771e0c38eedf3d3ffb7
NzkFalconet <khanmuzzama0786@gmail.com>
2022-10-29 12:18:58 +0530

    d

---
+++ names.js
@@ -0,0 +1,4 @@
+const name1="shaan"
+const name2="vatsal"
+
+module.exports= {name1,name2}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+
+// const sayHi= (name)=>{
+//     console.log(`My name is ${name}`)
+// }
+
+// sayHi(name1)
+// sayHi(name2)
